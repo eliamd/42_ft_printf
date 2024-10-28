3b66d37770b7feafd55d9e85614a848e09904b6a
Eliam <Texsword76@gmail.com>
2024-10-28 12:59:18 +0100

    ca comm a marcher

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,39 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:45:00 by edetoh            #+#    #+#             */
-/*   Updated: 2024/10/25 18:16:11 by edetoh           ###   ########.fr       */
+/*   Updated: 2024/10/28 12:57:07 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-static
+size_t	ft_strlen(const char *s)
+{
+	int	count;
+
+	count = 0;
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
+
+static int	ft_putchar(char c)
+{
+	if (write(1, &c, 1))
+		return (1);
+	return (0);
+}
+
+static int	ft_putstr(char *s)
+{
+	int	str_len;
+
+	str_len = ft_strlen(s);
+	write(1, s, str_len);
+	return (str_len);
+}
+
+
 
 static int	print_format(char format, va_list ap)
 {
@@ -20,19 +46,19 @@ static int	print_format(char format, va_list ap)
 
 	count = 0;
 	if (format == 'c')
-		count += ft_putchar(va_arg(ap, char)); // A FAIRE
+		count += ft_putchar(va_arg(ap, int));
 	if (format == 's')
-		count += ft_putstr(va_arg(ap, char *)); // A FAIRE
-	if (format == 'p')
-		count += ft_putstr(va_arg(ap, char *)); // A FAIRE
-	if (format == 'i')
-		count += ft_putint(va_arg(ap, char *));
-	if (format == 'x')
-		count += ft_putint(va_arg(ap, char *));
-	if (format == 'X')
-		count += ft_putint(va_arg(ap, char *));
-	if (format == '%')
-		count += ft_putint(va_arg(ap, char *));
+		count += ft_putstr(va_arg(ap, char *));
+	// if (format == 'p')
+	// 	count += ft_putstr(va_arg(ap, char *));
+	// if (format == 'i')
+	// 	count += ft_putint(va_arg(ap, char *));
+	// if (format == 'x')
+	// 	count += ft_putint(va_arg(ap, char *));
+	// if (format == 'X')
+	// 	count += ft_putint(va_arg(ap, char *));
+	// if (format == '%')
+	// 	count += ft_putint(va_arg(ap, char *));
 	return (count);
 }
 
@@ -53,4 +79,11 @@ int	ft_printf(const char *str, ...)
 	}
 	va_end(ap);
 	return (count);
+}
+
+int	main(void)
+{
+	ft_printf("MOi ; Bonjour %s, merci %s!\n", "CCCCCCC", "AAA");
+	printf("VRA ; Bonjour %s, merci %s!\n", "CCCCCCC", "AAA");
+	return (1);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 11:13:45 by edetoh            #+#    #+#             */
-/*   Updated: 2024/10/25 18:06:54 by edetoh           ###   ########.fr       */
+/*   Updated: 2024/10/28 12:56:50 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+# include <stdio.h>
 
 #endif
