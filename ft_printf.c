1ff1991a13a7fae1e746f83f0c96ddc5c62e85be
Eliam <Texsword76@gmail.com>
2024-10-28 14:24:50 +0100

    test

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:45:00 by edetoh            #+#    #+#             */
-/*   Updated: 2024/10/28 12:57:07 by edetoh           ###   ########.fr       */
+/*   Updated: 2024/10/28 14:22:39 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,44 @@ static int	ft_putstr(char *s)
 	return (str_len);
 }
 
+int	ft_putadress(void *ptr)
+{
+	uintptr_t		addr;
+	char			*hex_digits;
+	char			address[16];
+	int				i;
+	int				start;
 
+	hex_digits = "0123456789abcdef";
+	addr = (uintptr_t)ptr;
+	i = 15;
+	while (i >= 0)
+	{
+		address[i] = hex_digits[addr % 16];
+		addr /= 16;
+		i--;
+	}
+	write(1, "0x", 2);
+	start = 0;
+	while (start < 16 && address[start] == '0')
+		start++;
+	if (start == 16)
+		start = 15;
+	write(1, &address[start], 16 - start);
+	return (2 + 16 - start);
+}
 
-static int	print_format(char format, va_list ap)
+static int	print_format(char format, va_list *ap)
 {
 	int			count;
 
 	count = 0;
 	if (format == 'c')
-		count += ft_putchar(va_arg(ap, int));
+		count += ft_putchar(va_arg(*ap, int));
 	if (format == 's')
-		count += ft_putstr(va_arg(ap, char *));
-	// if (format == 'p')
-	// 	count += ft_putstr(va_arg(ap, char *));
+		count += ft_putstr(va_arg(*ap, char *));
+	if (format == 'p')
+		count += ft_putadress(va_arg(*ap, char *));
 	// if (format == 'i')
 	// 	count += ft_putint(va_arg(ap, char *));
 	// if (format == 'x')
@@ -59,6 +84,8 @@ static int	print_format(char format, va_list ap)
 	// 	count += ft_putint(va_arg(ap, char *));
 	// if (format == '%')
 	// 	count += ft_putint(va_arg(ap, char *));
+	else
+		write(1, &format, 1);
 	return (count);
 }
 
@@ -72,7 +99,13 @@ int	ft_printf(const char *str, ...)
 	while (*str != '\0')
 	{
 		if (*str == '%')
-			count += print_format(*(++str), ap);
+		{
+			str++;
+			if (*str != '\0')
+			{
+				count += print_format(*str, &ap);
+			}
+		}
 		else
 			count += write(1, str, 1);
 		str++;
@@ -83,7 +116,14 @@ int	ft_printf(const char *str, ...)
 
 int	main(void)
 {
-	ft_printf("MOi ; Bonjour %s, merci %s!\n", "CCCCCCC", "AAA");
-	printf("VRA ; Bonjour %s, merci %s!\n", "CCCCCCC", "AAA");
+	int resp;
+	int resv;
+
+	int *pointeur = &resp;
+
+	resv = ft_printf("=== MON PRINTF ===\nchar : %c\nString : %s\nPointeur : %p\n", 'Z', "Bonjour", (void *)pointeur);
+	resp = printf("=== VRA PRINTF ===\nchar : %c\nString : %s\nPointeur : %p\n", 'Z', "Bonjour", (void *)pointeur);
+
+	printf("\n%i = %i \n", resp, resv);
 	return (1);
 }
